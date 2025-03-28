aaf8d8d1cb018667016c98d0d0b1359fb2387203
SantiagouJ <santiagojuradorodriguez3@gmail.com>
2025-03-28 18:57:39 -0500

    Final commit

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,6 +1,6 @@
 class Component extends HTMLElement {
     static get observedAttributes(){
-        return ["destino","duracion","costo","descripcion","actividades","disponibilidad","img","calificacion","alojamiento","guia"]
+        return ["destino","duracion","costo","descripcion","actividades","disponibilidad","calificacion","alojamiento","guia"]
     }
 
     constructor(){
@@ -15,16 +15,30 @@ class Component extends HTMLElement {
 
     attributeChangedCallback(name, oldValue, newValue){
         if(oldValue !== newValue){
-            return[name]
+            this[name]=newValue
         }
     }
 
     render(){
         this.shadowRoot.innerHTML=`
+        
             <link rel="stylesheet" href="/src/components/card/card.css">
 
+            <div class="container">
+                <img src="https://picsum.photos/200/300" alt="">
 
-            <p>${this.destino}</p>
+                <div class="textSection">
+                    <p>${this.destino}</p>
+                    <p>${this.duracion}</p>
+                    <p>${this.costo}</p>
+                    <p>${this.descripcion}</p>
+                    <p>${this.actividades}</p>
+                    <p>${this.disponibilidad}</p>
+                    <p>${this.calificacion}</p>
+                    <p>${this.alojamiento}</p>
+                    <p>${this.guia}</p>
+                </div>
+            </div>
         
         `
     }
